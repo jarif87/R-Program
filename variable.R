37917cf2085aefedc4c8b14ef01506a8476348d6
Sadik Al Jarif <78945097+jarif87@users.noreply.github.com>
2023-02-21 01:59:46 +0600

    Add files via upload

---
+++ variable.R
@@ -0,0 +1,22 @@
+x<-"hell0"
+print(x)
+
+first_name<-"shakib"
+print(first_name)
+
+m.y<-"second"
+print(m.y)
+
+x<-25
+print(x)
+
+201->z
+print(z)
+
+
+
+y<-x==z
+print(y)
+
+cat(x," ",z,"\n",y)
+
